a48199e752992860721e98bba8b302c2628deb98
ablahum <mriztama@gmail.com>
2025-01-12 05:20:43 +0700

    refactor: conditional component on teacher's form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -43,7 +43,7 @@ const FormModal = ({
   id?: number;
 }) => {
   const size = type === 'create' ? 'w-8 h-8' : 'w-7 h-7';
-  const bgColor = type === 'create' ? 'bg-schYellow' : type === 'update' ? 'bg-schSky' : 'bg-schPurple';
+  const bgColor = type === 'create' ? 'bg-schYellow' : type === 'update' ? 'bg-schYellow' : 'bg-schPurple';
 
   const [open, setOpen] = useState(false);
 
@@ -79,10 +79,12 @@ const FormModal = ({
           height={16}
         />
       </button>
+
       {open && (
         <div className='w-screen h-screen absolute left-0 top-0 bg-black bg-opacity-60 z-50 flex items-center justify-center'>
           <div className='bg-white p-4 rounded-md relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]'>
             <Form />
+
             <div
               className='absolute top-4 right-4 cursor-pointer'
               onClick={() => setOpen(false)}
